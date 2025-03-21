5dfa7895ec20f1118251ae7e010deb4171b04729
Sedhupathi R <sedhupathi1411@gamil.com>
2025-03-21 15:08:55 +0530

    add: scroll animations

--- scripts.js
+++ scripts.js
@@ -26,3 +26,15 @@ function setMeAsActive(e) {
 }
 
 navAElements.forEach(e => e.onclick = e => setMeAsActive(e))
+
+
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) entry.target.classList.add("show");
+    else entry.target.classList.remove("show");
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".to-animate");
+hiddenElements.forEach(el => observer.observe(el));
